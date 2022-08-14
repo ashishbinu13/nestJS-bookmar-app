8da40c47ecc7f17e105b47bf209c3229438a1f7b
ashishbinu13 <ashish.binu1@gmail.com>
2022-08-14 18:02:56 +0530

    auth module setup + docker setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { BookmarksModule } from './bookmarks/bookmarks.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [AuthModule, UserModule, BookmarksModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
