bef05386c588450b7c7869a6259f57aaa96b98cc
ashishbinu13 <ashish.binu1@gmail.com>
2022-08-14 18:30:13 +0530

    prisma init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
-import { BookmarksModule } from './bookmarks/bookmarks.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarksModule],
+  imports: [AuthModule, UserModule, BookmarkModule],
   controllers: [],
   providers: [],
 })
